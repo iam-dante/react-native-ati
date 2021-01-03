bcea364275772603b656dc9899be22fb8dda2abb
iam-kevin <kevin.al.james@gmail.com>
2021-01-03 13:52:30 +0300

    massive push

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import {View, Text} from 'react-native'
+import LoginScreen from './app/views/LoginScreen'
+import SplashScreen from 'react-native-splash-screen'
+
+
+
+
+
+
+
+export default () => {
+    // const [doneLoading, setDoneLoading] = useState(false)
+
+    // useEffect(() => {
+    //     const abc = setTimeout(() => {
+    //         setDoneLoading(true)
+    //     }, 2000)
+
+    //     return clearTimeout(abc)
+    // }, [])
+
+    // if (doneLoading) {
+        SplashScreen.hide()
+    // }
+
+    return (
+        // <SplashScreen />
+        <LoginScreen />
+    );
+}
+
